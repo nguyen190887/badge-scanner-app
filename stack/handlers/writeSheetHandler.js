7ed13bf0610650e51d2975546aa84aa9787ecb8d
Tri-Nguyen <tri.nguyen@coxautoinc.com>
2019-11-29 10:44:56 +0700

    Fix issue: write to sheet

--- stack/handlers/writeSheetHandler.js
+++ stack/handlers/writeSheetHandler.js
@@ -1,7 +1,7 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
 
 module.exports.index = async (event) => {
-  console.log('Write Sheet Handler');
+  console.log('Write Sheet Handler', event);
     var doc = new GoogleSpreadsheet(process.env.SHEET_ID);
     var creds =
     {
@@ -16,11 +16,15 @@ module.exports.index = async (event) => {
         if (err) {
           console.log(err);
         }
+        const {topic: topicid, userId: userid} = event.arguments;
         doc.addRow(2, {
-          topic: event.queryStringParameters.topic,
-          userid: event.queryStringParameters.userid,
-        }, function () {
-          resolve({}); //todo
+          topicid,
+          userid,
+        }, function (data) {
+          console.info('Added', data);
+          resolve({
+            id: 0
+          }); //todo
           return {};
         });
       });
