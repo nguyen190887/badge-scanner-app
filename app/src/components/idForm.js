fa0cd8c45f728ac70e91a63b9c17418cd6ef474d
sollicitudin <rosmuseing@gmail.com>
2019-12-03 14:32:36 +0700

    subscriptions for topic attendance

--- app/src/components/idForm.js
+++ app/src/components/idForm.js
@@ -1,12 +1,14 @@
 import React, { useRef } from 'react';
+import API, { graphqlOperation } from '@aws-amplify/api';
+import { addTrackingRow } from '../graphql/mutations';
 
-export default ({ onCreate, topicId }) => {
+export default ({ topicId }) => {
   const inputRef = useRef(null);
 
   const onSubmit = async e => {
     e.preventDefault();
     try {
-      await onCreate({ id: topicId, userId: inputRef.current.value });
+      await API.graphql(graphqlOperation(addTrackingRow, { id: topicId, userId: inputRef.current.value }))
       inputRef.current.value = '';
     } catch (err) {
       console.error(err);
