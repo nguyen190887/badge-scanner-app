38e7ff9cb94eeec59a80edb44e7282f4d1027fd6
sollicitudin <rosmuseing@gmail.com>
2019-12-11 09:01:43 +0700

    remove unused packages

--- app/src/apollo/client.js
+++ app/src/apollo/client.js
@@ -1,21 +1,19 @@
 import fetch from 'isomorphic-fetch';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, defaultDataIdFromObject } from 'apollo-cache-inmemory';
-import { createHttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
 import { ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
+import { onError } from 'apollo-link-error';
+import { createHttpLink } from 'apollo-link-http';
 import { toIdValue } from 'apollo-utilities';
-import { createAuthLink } from 'aws-appsync-auth-link';
-import { createSubscriptionHandshakeLink } from 'aws-appsync-subscription-link';
 
 const cacheInstance = new InMemoryCache({
   addTypename: true,
   dataIdFromObject: object => {
-    switch (object.__typename) {
-      case 'Topic': return `Topic:${object.topicId}`;
-      case 'TrackingRow': return `TrackingRow:${object.topicId}_${object.userId}`;
-      default: return defaultDataIdFromObject(object);
+    if (object.topicId || object.userId) {
+      return `${object.__typename}${object.topicId ? `:${object.topicId}` : ''}${object.userId ? `:${object.userId}` : ''}`
     }
+    return defaultDataIdFromObject(object);
   },
   cacheRedirects: {
     Query: {
@@ -26,10 +24,17 @@ const cacheInstance = new InMemoryCache({
 
 const cache = (typeof window === 'undefined') ? cacheInstance : cacheInstance.restore(window.__APOLLO_STATE__);
 
-const url = `${process.env.APPSYNC_ENDPOINT}`;
-
 const httpLink = createHttpLink({
-  uri: url,
+  uri: `${process.env.APPSYNC_ENDPOINT}`
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      'X-Api-Key': `${process.env.APPSYNC_API_KEY}`,
+    }
+  }
 });
 
 const link = ApolloLink.from([
@@ -42,15 +47,7 @@ const link = ApolloLink.from([
       );
     if (networkError) console.log(`[Network error]: ${networkError}`);
   }),
-  createAuthLink({
-    url,
-    region: `${process.env.REGION}`,
-    auth: {
-      type: 'API_KEY',
-      apiKey: `${process.env.APPSYNC_API_KEY}`
-    }
-  }),
-  createSubscriptionHandshakeLink(url, httpLink)
+  authLink.concat(httpLink)
 ]);
 
 export const client = new ApolloClient({
