2f9a792ee85447047f0729b9e59916d7da96638b
sollicitudin <rosmuseing@gmail.com>
2019-12-13 12:35:50 +0700

    hotfix | responsive topic table

--- app/src/components/topicList.js
+++ app/src/components/topicList.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { Paper, Table, TableBody, TableRow, TableHead, TableCell } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableHead from '@material-ui/core/TableHead';
+import TableCell from '@material-ui/core/TableCell';
+import Hidden from '@material-ui/core/Hidden';
 import { StyledTableRow } from './table';
 
 const TopicList = ({ topics: { allTopics = [] } = {} }) => {
@@ -9,13 +15,17 @@ const TopicList = ({ topics: { allTopics = [] } = {} }) => {
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
+            <Hidden smDown>
+              <TableCell>Date</TableCell>
+            </Hidden>
             <TableCell>Topic</TableCell>
             <TableCell>Owner</TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell>SME Group</TableCell>
-            <TableCell>Duration</TableCell>
-            <TableCell>Notes</TableCell>
+            <Hidden smDown>
+              <TableCell>Status</TableCell>
+              <TableCell>SME Group</TableCell>
+              <TableCell>Duration</TableCell>
+              <TableCell>Notes</TableCell>
+            </Hidden>
             <TableCell />
           </TableRow>
         </TableHead>
@@ -23,13 +33,17 @@ const TopicList = ({ topics: { allTopics = [] } = {} }) => {
           {
             allTopics.map((topic) => (
               <StyledTableRow key={topic.topicId}>
-                <TableCell>{topic.date}</TableCell>
+                <Hidden smDown>
+                  <TableCell>{topic.date}</TableCell>
+                </Hidden>
                 <TableCell>{topic.name}</TableCell>
                 <TableCell>{topic.owner}</TableCell>
-                <TableCell>{topic.status}</TableCell>
-                <TableCell>{topic.smeGroup}</TableCell>
-                <TableCell>{topic.duration}</TableCell>
-                <TableCell>{topic.notes}</TableCell>
+                <Hidden smDown>
+                  <TableCell>{topic.status}</TableCell>
+                  <TableCell>{topic.smeGroup}</TableCell>
+                  <TableCell>{topic.duration}</TableCell>
+                  <TableCell>{topic.notes}</TableCell>
+                </Hidden>
                 <TableCell><Link to={`/topic/${topic.topicId}`}>Detail</Link></TableCell>
               </StyledTableRow>
             ))
