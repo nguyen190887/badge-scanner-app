0a6becbd2695c52f71d144f5e4dfde1a36d1b5a4
nguyen190887 <nguyen190887@gmail.com>
2019-12-20 06:37:14 +0700

    Install aws-cli via alpine packages; try to config serverless

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -4,6 +4,8 @@ sh -c "echo Hello world. My name is $MY_NAME"
 
 ls -la
 
+serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
+
 export AWS_ACCESS_KEY_ID=p"$AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
 
