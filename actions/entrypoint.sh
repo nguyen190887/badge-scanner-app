ff2aebd248f0c665891af486d370addedc30042d
nguyen190887 <nguyen190887@gmail.com>
2019-12-19 08:01:06 +0700

    Install depedencies; expose AWS credentials

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -4,6 +4,9 @@ sh -c "echo Hello world. My name is $MY_NAME"
 
 ls -la
 
+export AWS_ACCESS_KEY_ID=p"$AWS_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
+
 STAGE="$BRANCH_NAME"
 if [ "$BRANCH_NAME" = "master" ]; then
     STAGE="prod"
