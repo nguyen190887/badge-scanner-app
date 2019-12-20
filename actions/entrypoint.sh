9970e080e788dc9d5ec95994845eb9459ac9ecf1
nguyen190887 <nguyen190887@gmail.com>
2019-12-20 09:24:04 +0700

    Add serverless credentials; try to fix aws cli

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -4,10 +4,11 @@ sh -c "echo Hello world. My name is $MY_NAME"
 
 ls -la
 
-serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
+# serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
 
 export AWS_ACCESS_KEY_ID=p"$AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
+export SERVERLESS_ACCESS_KEY="$SERVERLESS_ACCESS_KEY"
 
 STAGE="$BRANCH_NAME"
 if [ "$BRANCH_NAME" = "master" ]; then
