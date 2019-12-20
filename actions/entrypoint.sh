67ded30171ec080847f468a0fa175f91202a0374
nguyen190887 <nguyen190887@gmail.com>
2019-12-20 20:09:07 +0700

    Clean up; try remove sls key

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,22 +1,6 @@
 #!/bin/sh -l
-
-# aws s3 ls
-
-echo "Access key: $AWS_ACCESS_KEY_ID"
-echo "Secret key: $AWS_SECRET_ACCESS_KEY"
-echo "Serverless key: $SERVERLESS_ACCESS_KEY"
-
-# aws --version
-
-# export AWS_REGION="us-east-1"
-# # export AWS_ACCESS_KEY_ID=p"$AWS_ACCESS_KEY_ID"
-# # export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
-# # export SERVERLESS_ACCESS_KEY="$SERVERLESS_ACCESS_KEY"
-
-serverless --version
 serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
 
-# BRANCH_NAME="${BRANCH_NAME/eng:/}"
 STAGE="$BRANCH_NAME"
 if [ "$BRANCH_NAME" = "master" ]; then
     STAGE="prod"
