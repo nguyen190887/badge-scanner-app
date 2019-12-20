942d711a6aa5a6c958df891fbe0bf3a7f7d3463c
nguyen190887 <nguyen190887@gmail.com>
2019-12-20 12:17:47 +0700

    Use with

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -5,6 +5,7 @@ echo "Serverless key: $SERVERLESS_ACCESS_KEY"
 
 aws --version
 
+export AWS_REGION="us-east-1"
 export AWS_ACCESS_KEY_ID=p"$AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
 export SERVERLESS_ACCESS_KEY="$SERVERLESS_ACCESS_KEY"
@@ -12,7 +13,7 @@ export SERVERLESS_ACCESS_KEY="$SERVERLESS_ACCESS_KEY"
 serverless --version
 serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
 
-BRANCH_NAME="${BRANCH_NAME/eng:/}"
+# BRANCH_NAME="${BRANCH_NAME/eng:/}"
 STAGE="$BRANCH_NAME"
 if [ "$BRANCH_NAME" = "master" ]; then
     STAGE="prod"
