29eeb47fcee4f01ab1d117e06f2479830a9db4c7
nguyen190887 <nguyen190887@gmail.com>
2019-12-20 10:08:50 +0700

    Change docker image

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,19 +1,15 @@
 #!/bin/sh -l
+echo "Access key: $AWS_ACCESS_KEY_ID"
+echo "Secret key: $AWS_SECRET_ACCESS_KEY"
+echo "Serverless key: $SERVERLESS_ACCESS_KEY"
 
-sh -c "echo Hello world. My name is $MY_NAME"
-
-ls -la
-
-# serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-echo $SERVERLESS_ACCESS_KEY
+aws --version
 
 export AWS_ACCESS_KEY_ID=p"$AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
 export SERVERLESS_ACCESS_KEY="$SERVERLESS_ACCESS_KEY"
 
-serverless login
+serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
 
 STAGE="$BRANCH_NAME"
 if [ "$BRANCH_NAME" = "master" ]; then
