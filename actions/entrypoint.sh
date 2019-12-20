19173f73b64ed39a89951438b76950c1f8170b3a
nguyen190887 <nguyen190887@gmail.com>
2019-12-20 13:41:02 +0700

    Test credentials

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,23 +1,26 @@
 #!/bin/sh -l
-echo "Access key: $AWS_ACCESS_KEY_ID"
-echo "Secret key: $AWS_SECRET_ACCESS_KEY"
-echo "Serverless key: $SERVERLESS_ACCESS_KEY"
 
-aws --version
+aws s3 ls
 
-export AWS_REGION="us-east-1"
-# export AWS_ACCESS_KEY_ID=p"$AWS_ACCESS_KEY_ID"
-# export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
-# export SERVERLESS_ACCESS_KEY="$SERVERLESS_ACCESS_KEY"
+# echo "Access key: $AWS_ACCESS_KEY_ID"
+# echo "Secret key: $AWS_SECRET_ACCESS_KEY"
+# echo "Serverless key: $SERVERLESS_ACCESS_KEY"
 
-serverless --version
-serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
+# aws --version
 
-# BRANCH_NAME="${BRANCH_NAME/eng:/}"
-STAGE="$BRANCH_NAME"
-if [ "$BRANCH_NAME" = "master" ]; then
-    STAGE="prod"
-fi
+# export AWS_REGION="us-east-1"
+# # export AWS_ACCESS_KEY_ID=p"$AWS_ACCESS_KEY_ID"
+# # export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
+# # export SERVERLESS_ACCESS_KEY="$SERVERLESS_ACCESS_KEY"
 
-. deploy.sh $STAGE
+# serverless --version
+# serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
+
+# # BRANCH_NAME="${BRANCH_NAME/eng:/}"
+# STAGE="$BRANCH_NAME"
+# if [ "$BRANCH_NAME" = "master" ]; then
+#     STAGE="prod"
+# fi
+
+# . deploy.sh $STAGE
 
