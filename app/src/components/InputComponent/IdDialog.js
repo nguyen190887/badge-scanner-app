98c6680ecf677ea9d65492ee2e752e912cd60375
sollicitudin <rosmuseing@gmail.com>
2020-01-15 09:33:51 +0700

    optimisticResponse :metal:

--- app/src/components/InputComponent/IdDialog.js
+++ app/src/components/InputComponent/IdDialog.js
@@ -1,14 +1,10 @@
 import React, { useRef, useState } from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import { makeStyles } from '@material-ui/core/styles';
 import InputTabs from './InputTabs';
 
@@ -30,7 +26,22 @@ export default ({ topicId, addRow, open, setOpen }) => {
 
   const handleSubmit = () => {
     try {
-      addRow({ variables: { topicId, userId: idRef.current.value, userName: userNameRef.current.value } })
+      addRow({
+        variables: { topicId, userId: idRef.current.value, userName: userNameRef.current.value },
+        optimisticResponse: {
+          __typename: "Mutation",
+          addTrackingRow: {
+            __typename: "TrackingRow",
+            comment: "",
+            email: "",
+            imagePath: "",
+            rating: "",
+            topicId: topicId,
+            userId: ("0000" + idRef.current.value).slice(-4),
+            userName: userNameRef.current.value
+          }
+        },
+      })
     } catch (err) {
       console.error(err);
     }
--- app/src/components/InputComponent/IdForm.js
+++ app/src/components/InputComponent/IdForm.js
@@ -2,15 +2,11 @@ import React, { useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import InputTabs from './InputTabs';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-    },
+  wrapper: {
+    margin: theme.spacing(2, 0)
   },
   box: {
     maxWidth: '400px'
@@ -28,7 +24,22 @@ export default ({ topicId, addRow }) => {
   const onSubmit = async e => {
     e.preventDefault();
     try {
-      addRow({ variables: { topicId, userId: idRef.current.value, userName: userNameRef.current.value } })
+      addRow({
+        variables: { topicId, userId: idRef.current.value, userName: userNameRef.current.value },
+        optimisticResponse: {
+          __typename: "Mutation",
+          addTrackingRow: {
+            __typename: "TrackingRow",
+            comment: "",
+            email: "",
+            imagePath: "",
+            rating: "",
+            topicId: topicId,
+            userId: ("0000" + idRef.current.value).slice(-4),
+            userName: userNameRef.current.value
+          }
+        },
+      })
       idRef.current.value = '';
       userNameRef.current.value = '';
     } catch (err) {
@@ -37,11 +48,11 @@ export default ({ topicId, addRow }) => {
   };
 
   return (
-    <Box display='flex'>
+    <Box display='flex' className={classes.wrapper}>
       <Box className={classes.box}>
         <InputTabs idRef={idRef} userNameRef={userNameRef} />
       </Box>
-      <Button type='submit' variant='text' className={classes.button}>Submit</Button>
+      <Button onClick={onSubmit} type='submit' variant='text' className={classes.button}>Submit</Button>
     </Box>
   );
 };
