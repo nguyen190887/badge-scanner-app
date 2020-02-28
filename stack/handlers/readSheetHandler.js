a453e94c5162931a67357960e8ec5ff010486db8
Nguyen Thao <rosmuseing@gmail.com>
2020-02-28 12:10:33 +0700

    Update google-spreadsheet to v3.0 (#99)
    * update google-spreadsheet to 3.0
    
    because Google deprecated their v3 sheets API on March 3, 2020. This module warns users to upgrade to newer version with a lot of breaking change.
    
    * pick common mapping object function to utils

--- stack/handlers/readSheetHandler.js
+++ stack/handlers/readSheetHandler.js
@@ -1,77 +1,35 @@
-const GoogleSpreadsheet = require('google-spreadsheet');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { mapAttendanceSheetRowToObject, mapTopicSheetRowToObject } = require('./utils');
 
 const readTopics = async (doc) => {
-  return new Promise(resolve => {
-    doc.getRows(1, function (err, rows) {
-      let response = [];
-      rows.forEach(function (row) {
-        response.push({
-          topicId: row.no,
-          date: row.date,
-          name: row.name,
-          owner: row.owner,
-          status: row.status,
-          smeGroup: row.smegroup,
-          duration: row.duration,
-          notes: row.notes,
-        });
-      });
-      resolve(response.sort((a,b) => {
-        return new Date(b.date) - new Date(a.date);
-      }));
-    });
+  const sheet = doc.sheetsByIndex[0];
+  const rows = await sheet.getRows();
+  const result = [];
+  rows.forEach(row => result.push(mapTopicSheetRowToObject(row)))
+  result.sort((a, b) => {
+    return new Date(b.date) - new Date(a.date);
   });
-}
+  return result;
+};
 
 const readTopic = async (doc, args) => {
-  return new Promise(resolve => {
-    const { topicId } = args;
-    doc.getRows(1, {
-      query: `(no=${topicId})`
-    }, function (err, rows) {
-      if (rows) {
-        resolve({
-          topicId: rows[0].no,
-          date: rows[0].date,
-          name: rows[0].name,
-          owner: rows[0].owner,
-          status: rows[0].status,
-          smeGroup: rows[0].smegroup,
-          duration: rows[0].duration,
-          notes: rows[0].notes,
-        });
-      }
-      else {
-        resolve({});
-      }
-    });
-  });
-}
+  const { topicId } = args;
+  const sheet = doc.sheetsByIndex[0];
+  const rows = await sheet.getRows();
+  const row = rows.find(row => row.No === `${topicId}`);
+  return mapTopicSheetRowToObject(row);
+};
 
-const readTopicAttendance = async (doc = new GoogleSpreadsheet(), args) => {
-  return new Promise(resolve => {
-    const { topicId } = args;
-    doc.getRows(2, {
-      query: `(topicid=${topicId})`
-    }, function (_err, rows) {
-      console.info(rows);
-      let response = [];
-      rows.forEach(row => {
-        response.push({
-          topicId: row.topicid,
-          userId: row.userid,
-          userName: row.username,
-          email: row.email,
-          imagePath: row.imagepath,
-          rating: row.rating,
-          comment: row.comment,
-          userName: row.username
-        })
-      });
-      resolve(response);
-    });
-  });
-}
+// TODO: limit fetched row with each requests
+const readTopicAttendance = async (doc, args) => {
+  const { topicId } = args;
+  const sheet = doc.sheetsByIndex[1];
+  const rows = await sheet.getRows();
+  const attendance = rows.filter(row => row['Topic ID'] === `${topicId}`);
+  const result = [];
+  attendance.forEach(row => result.push(mapAttendanceSheetRowToObject(row)));
+  return result;
+};
 
 const fieldMapping = {
   getAllTopics: readTopics,
@@ -83,27 +41,15 @@ module.exports.index = async event => {
   console.log('Read Sheet Handler');
   console.log(event.field);
 
-  var doc = new GoogleSpreadsheet(
-    process.env.SHEET_ID
-  );
-  var creds = {
+  const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
+  const creds = {
     private_key: process.env.PRIVATE_KEY,
     client_email: process.env.CLIENT_EMAIL,
   };
+  await doc.useServiceAccountAuth(creds);
+  await doc.loadInfo();
 
-  const promise = new Promise(resolve => {
-    doc.useServiceAccountAuth(creds, async function (err) {
-      if (err) {
-        console.error(err);
-      }
-
-      if (fieldMapping[event.field]) {
-        resolve(await fieldMapping[event.field](doc, event.arguments));
-        return;
-      }
-
-      resolve([]);
-    });
-  });
-  return promise;
+  if (fieldMapping[event.field]) {
+    return await fieldMapping[event.field](doc, event.arguments);
+  }
 };
